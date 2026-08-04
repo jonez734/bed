fc2c9e3416593b94edd233286dc656a983b9fcc1
J (eff) <jam@zoidtechnologies.com>
2026-08-03 20:42:02 -0400

    bed: make install runs version and rebuilds with --no-cache-dir

--- src/bed/_version.py
+++ src/bed/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202607211701"
-__datestamp__ = "202607211701"
-__githash__ = "0fa98b69e0c03fa8"
+__version__ = "0.0.1.dev202608032039"
+githash = "464f745d13017e3a"
+datestamp = "202608032039"
