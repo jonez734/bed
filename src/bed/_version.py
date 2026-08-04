9495512e929cafd7384d718240dbd40ed83c9fed
J (eff) <jam@zoidtechnologies.com>
2026-08-04 19:18:40 -0400

    bed: drop TestBedJsonModuleImports; zoid6 config validation belongs in zoid6 tests

--- src/bed/_version.py
+++ src/bed/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608032047"
-githash = "dfde02b47d10364a"
-datestamp = "202608032047"
+__version__ = "0.0.1.dev202608041848"
+__datestamp__ = "202608041848"
+__githash__ = "10bd7717416039f8"
