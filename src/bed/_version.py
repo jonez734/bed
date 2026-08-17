e046dd385608dc81c8956889e1b3e2a4e3481f05
J (eff) <jam@zoidtechnologies.com>
2026-08-17 17:34:38 -0400

    chore: bump bed _version.py

--- src/bed/_version.py
+++ src/bed/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608162121"
-__datestamp__ = "202608162121"
-__githash__ = "fab076d8eacdbded"
+__version__ = "0.0.1.dev202608171602"
+__datestamp__ = "202608171602"
+__githash__ = "71060c9d3efc7303"
