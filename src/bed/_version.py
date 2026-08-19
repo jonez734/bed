5bdde0cefe099783f9581ea028329536b30e9158
J (eff) <jam@zoidtechnologies.com>
2026-08-18 22:42:27 -0400

    chore(bed): bump _version.py after no-sudo install-user validation
    make install-user runs `make -C $(BED_DIR) version` which stamps
    bed/_version.py with a new datestamp and githash. This commit
    captures the stamp produced by the validation cycle.

--- src/bed/_version.py
+++ src/bed/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608171907"
-__datestamp__ = "202608171907"
-__githash__ = "e046dd385608dc81"
+__version__ = "0.0.1.dev202608182237"
+__datestamp__ = "202608182237"
+__githash__ = "2fb546bb1d8ac773"
