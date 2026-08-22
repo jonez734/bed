c772a6de294f1b9ebb96081ce47ff7127f24c3da
J (eff) <jam@zoidtechnologies.com>
2026-08-22 12:37:45 -0400

    chore(bed): bump _version.py to 202608221229
    Auto-regenerated by 'make version' on 2026-08-22 12:29 UTC.

--- src/bed/_version.py
+++ src/bed/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608211924"
-__datestamp__ = "202608211924"
-__githash__ = "da0cf99715cfba38"
+__version__ = "0.0.1.dev202608221229"
+__datestamp__ = "202608221229"
+__githash__ = "36eea20c844d5c45"
