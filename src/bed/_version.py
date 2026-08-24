7177bf229f5697a17522b1ec95744615255486c2
J (eff) <jam@zoidtechnologies.com>
2026-08-24 09:35:05 -0400

    chore(bed): bump _version.py to 202608240912

--- src/bed/_version.py
+++ src/bed/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608222101"
-__datestamp__ = "202608222101"
-__githash__ = "db4a4afce3b70b4c"
+__version__ = "0.0.1.dev202608240912"
+__datestamp__ = "202608240912"
+__githash__ = "fe27525b62e43df5"
