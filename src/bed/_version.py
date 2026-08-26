8ab29ce6cdaee5248a21674051cbb7ef0c90b3d0
J (eff) <jam@zoidtechnologies.com>
2026-08-25 23:02:55 -0400

    bed: bump _version.py

--- src/bed/_version.py
+++ src/bed/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608240912"
-__datestamp__ = "202608240912"
-__githash__ = "fe27525b62e43df5"
+__version__ = "0.0.1.dev202608252143"
+__datestamp__ = "202608252143"
+__githash__ = "eb5b6bfebc18be8c"
